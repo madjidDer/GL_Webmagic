d92251c5f3683a0a351905d8f5caabd61c3dd953
madjidDer <dermelmadjid@gmail.com>
2024-04-03 06:46:39 +0200

    Correction et amélioration de SpiderTest

--- webmagic/webmagic-core/src/test/java/us/codecraft/webmagic/SpiderTest.java
+++ webmagic/webmagic-core/src/test/java/us/codecraft/webmagic/SpiderTest.java
@@ -1,7 +1,10 @@
 package us.codecraft.webmagic;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import static org.junit.Assert.*;
 import us.codecraft.webmagic.downloader.Downloader;
 import us.codecraft.webmagic.pipeline.Pipeline;
 import us.codecraft.webmagic.processor.PageProcessor;
@@ -12,77 +15,54 @@ import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * @author code4crafter@gmail.com
+ * Tests for Spider.
  */
 public class SpiderTest {
 
+    private Spider spider;
+
+    @Before
+    public void setUp() {
+        spider = Spider.create(new SimplePageProcessor("http://www.oschina.net/*"));
+    }
     @Ignore("long time")
     @Test
     public void testStartAndStop() throws InterruptedException {
-        Spider spider = Spider.create(new SimplePageProcessor( "http://www.oschina.net/*")).addPipeline(new Pipeline() {
+        spider.addPipeline(new Pipeline() {
             @Override
             public void process(ResultItems resultItems, Task task) {
-                System.out.println(1);
+                assertTrue("Pipeline process should be executed", true);
             }
         }).thread(1).addUrl("http://www.oschina.net/");
         spider.start();
-        Thread.sleep(10000);
         spider.stop();
-        Thread.sleep(10000);
         spider.start();
-        Thread.sleep(10000);
     }
 
     @Ignore("long time")
     @Test
-    public void testWaitAndNotify() throws InterruptedException {
-        for (int i = 0; i < 10000; i++) {
-            System.out.println("round " + i);
-            testRound();
-        }
-    }
-
-    private void testRound() {
-        Spider spider = Spider.create(new PageProcessor() {
-
-            private AtomicInteger count = new AtomicInteger();
-
-            @Override
-            public void process(Page page) {
-                page.setSkip(true);
-            }
-
-            @Override
-            public Site getSite() {
-                return Site.me().setSleepTime(0);
-            }
-        }).setDownloader(new Downloader() {
+    public void testWaitAndNotify() {
+        spider.setDownloader(new Downloader() {
             @Override
             public Page download(Request request, Task task) {
                 return new Page().setRawText("");
             }
-
             @Override
             public void setThread(int threadNum) {
-
             }
         }).setScheduler(new Scheduler() {
-
             private AtomicInteger count = new AtomicInteger();
-
             private Random random = new Random();
 
             @Override
             public void push(Request request, Task task) {
-
             }
-
             @Override
             public synchronized Request poll(Task task) {
                 if (count.incrementAndGet() > 1000) {
                     return null;
                 }
-                if (random.nextInt(100)>90){
+                if (random.nextInt(100) > 90) {
                     return null;
                 }
                 return new Request("test");
