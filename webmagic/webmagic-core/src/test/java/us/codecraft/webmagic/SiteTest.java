4968b9c0da2d5a184fdfe9ad8fef8131d28bf508
madjidDer <dermelmadjid@gmail.com>
2024-04-03 06:59:38 +0200

    Amélioration des tests de la classe Site

--- webmagic/webmagic-core/src/test/java/us/codecraft/webmagic/SiteTest.java
+++ webmagic/webmagic-core/src/test/java/us/codecraft/webmagic/SiteTest.java
@@ -5,8 +5,11 @@ import static org.junit.Assert.assertTrue;
 
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.junit.Test;
 
@@ -36,5 +39,47 @@ public class SiteTest {
         assertEquals("cookie-webmagicCopy", allCookies.get(firstDomain).get("cookieCopy"));
         assertEquals("cookie-webmagic", allCookies.get(secondDomain).get("cookie"));
         assertEquals(2, domains.size());
+    }
+    
+    @Test
+    public void shouldAddCustomHeaderCorrectly() {
+        Site site = Site.me();
+        String headerName = "HeaderName";
+        String headerValue = "SomeValue";
+
+        site.addHeader(headerName, headerValue);
+
+        Map<String, String> headers = site.getHeaders();
+        assertEquals(headerValue, headers.get(headerName));
+    }
+    
+    @Test
+    public void shouldSetUserAgentCorrectly() {
+        Site site = Site.me();
+        String userAgentValue = "Mozilla/5.0";
+
+        site.setUserAgent(userAgentValue);
+
+        assertEquals("Le User-Agent doit être configuré correctement", userAgentValue, site.getUserAgent());
+    }
+    
+    @Test
+    public void shouldSetTimeoutCorrectly() {
+        Site site = Site.me();
+        int timeoutValue = 20000; // 20 secondes
+
+        site.setTimeOut(timeoutValue);
+
+        assertEquals("Le timeout doit être configuré correctement", timeoutValue, site.getTimeOut());
+    }
+    
+    @Test
+    public void shouldSetAcceptStatusCodeCorrectly() {
+        Site site = Site.me();
+        Set<Integer> acceptStatusCodes = new HashSet<>(Arrays.asList(200, 404));
+
+        site.setAcceptStatCode(acceptStatusCodes);
+
+        assertTrue("Le site doit être configuré pour accepter les codes de statut spécifiés", site.getAcceptStatCode().containsAll(acceptStatusCodes));
     }
 }
